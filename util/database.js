a948f6f5b938c14e7869c59a7e70a5b918c76767
Pratik Kumar <71834495+pratik-parascout@users.noreply.github.com>
2025-05-19 16:02:23 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    'uri'
   )
     .then(client => {
       console.log('Connected!');
